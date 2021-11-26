eb8db273bf65f08d906dccc539d02d8b436c587f
Alexander <79713962+dezhaviu@users.noreply.github.com>
2021-11-26 19:10:20 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const buttonElement = document.getElementById('btn');
-buttonElement.addEventListener('click', function (event) {
-  alert('Okeey');
-});
-buttonElement.addEventListener('click', {
-  handleEvent: function (event) {
-    alert('Let`s go!');
-  }
-});
